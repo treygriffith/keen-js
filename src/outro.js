509711cf7b1f2ef606df780b15e2afdf696aee76
Dustin Larimer <dustinlarimer@gmail.com>
2014-03-15 12:37:57 -0700

    Add jquery.documentReady hook to Keen.ready callback

--- src/outro.js
+++ src/outro.js
@@ -2,12 +2,20 @@
   // ----------------------
   // Utility Methods
   // ----------------------
-
+  
+  Keen.domReady = (typeof $ == 'function') ? $ : function(cb){ cb(); };
+  
+  Keen.ready = function(callback){
+    Keen.on('ready', Keen.domReady(function(){
+      callback();
+    }));
+  };
+  
+  Keen.trigger('ready');
+  
   Keen.log = function(message) {
     console.log('[Keen IO]', message)
   };
-
-  Keen.trigger('ready');
-
+  
   return Keen;
 });
